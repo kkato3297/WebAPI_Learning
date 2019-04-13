5518793aab0c0dee8a5e69e9681df6927f17431a
kkato3297 <mwo.kk.timp@gmail.com>
2019-04-14 00:23:53 +0900

    <mod> #19 記事を更新/削除してみよう

--- Express_Introduction/blog/app.js
+++ Express_Introduction/blog/app.js
@@ -12,7 +12,14 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+app.use(methodOverride(function(req, res){
+	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+		// URLエンコードされたPOSTボディを見て、削除する
+		var method = req.body._method;
+		delete req.body._method;
+		return method;
+	}
+}));
 
 app.use(logger('dev'));
 
@@ -23,11 +30,8 @@ app.get('/posts/:id([0-9]+)', post.show);
 app.get('/posts/new', post.new);
 app.post('/posts/create', post.create);
 app.get('/posts/:id/edit', post.edit);
-/*
-app.get('/posts/:id/edit', post.edit);
 app.put('/posts/:id', post.update);
 app.delete('/posts/:id', post.destroy);
-*/
 
 app.listen(3000);
 console.log("server starting...");
--- Express_Introduction/blog/routes/post.js
+++ Express_Introduction/blog/routes/post.js
@@ -16,6 +16,17 @@ exports.new = function(req, res) {
 exports.edit = function(req, res) {
 	res.render('posts/edit', {post: posts[req.params.id], id: req.params.id});
 };
+exports.update = function(req, res) {
+	posts[req.body.id] = {
+		title: req.body.title,
+		body: req.body.body
+	};
+	res.redirect('/');
+};
+exports.destroy = function(req, res) {
+	posts.splice(req.body.id, 1);
+	res.redirect('/');
+};
 exports.create = function(req, res) {
 	var post = {
 		title: req.body.title,
