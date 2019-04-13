86f995861ad4eabbeab8a2f2f65fae0ab3e61a79
kkato3297 <mwo.kk.timp@gmail.com>
2019-04-13 20:13:35 +0900

    <mod> #04 プレースホルダーを使おう

--- Express_Introduction/myapp/app.js
+++ Express_Introduction/myapp/app.js
@@ -6,11 +6,15 @@ var express = require('express'),
  * app.use(app.router);
  */
 
-app.get('/', function(req, res) {
-	res.send('hello world');
+app.get('/users/:name?', function(req, res) {
+	if (req.params.name) {
+		res.send('hello, ' + req.params.name);
+	} else {
+		res.send('hello, nobody!');
+	}
 });
-app.get('/about', function(req, res) {
-	res.send('about this page!');
+app.get('/items/:id([0-9]+)', function(req, res) {
+	res.send('item no: ' + req.params.id);
 });
 
 app.listen(3000);
