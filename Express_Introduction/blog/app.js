b7e81fc0b3f80cfa37b1864d964e8506606d7de9
kkato3297 <mwo.kk.timp@gmail.com>
2019-04-13 22:48:08 +0900

    <mod> #12 ルーティングを確認しよう

--- Express_Introduction/blog/app.js
+++ Express_Introduction/blog/app.js
@@ -2,7 +2,9 @@ var express = require('express'),
     app = express(),
     post = require('./routes/post'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    connect = require('connect'),
+    methodOverride = require('method-override');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -10,14 +12,19 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.use(logger('dev'));
 
 // routing
 
-app.get('/new', function(req, res) {
-	res.render('new');
-});
+app.get('/', post.index);
+app.get('/posts/new', post.new);
+app.post('/posts/create', post.create);
+app.get('/posts/:id', post.show);
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id', post.update);
+app.delete('/posts/:id', post.destroy);
 
 app.listen(3000);
 console.log("server starting...");
