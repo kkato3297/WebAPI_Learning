7726494d146370f118c281b4d6caa4823d3928db
kkato3297 <mwo.kk.timp@gmail.com>
2019-04-13 20:42:18 +0900

    <mod> #07 middlewareを作ってみよう

--- Express_Introduction/myapp/app.js
+++ Express_Introduction/myapp/app.js
@@ -1,12 +1,19 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    logger = require('morgan');	// Express 4.x ではexpress.logger()は使えなくなっているので、morganを使用してログを出力する。
 
 // middleware
+app.use(logger('dev'));
 // Express 4.xでは不要
 /*
+ * app.use(express.logger('dev'));
  * app.use(app.router);
  */
 app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+	console.log('my custom middleware !');
+	next();
+});
 
 app.get('/hello.txt', function(req, res) {
 	res.send('hello from app.js');
