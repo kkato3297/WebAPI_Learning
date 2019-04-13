07f4bdefab8bd93103c065f15b5a124107a965f4
kkato3297 <mwo.kk.timp@gmail.com>
2019-04-13 20:28:30 +0900

    <mod> #06 middlewareを使ってみよう

--- Express_Introduction/myapp/app.js
+++ Express_Introduction/myapp/app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express();
 
+// middleware
 // Express 4.xでは不要
 /*
  * app.use(app.router);
@@ -8,7 +9,7 @@ var express = require('express'),
 app.use(express.static(__dirname + '/public'));
 
 app.get('/hello.txt', function(req, res) {
-	res.sendFile(__dirname + '/public/hello.txt');
+	res.send('hello from app.js');
 });
 
 app.listen(3000);
